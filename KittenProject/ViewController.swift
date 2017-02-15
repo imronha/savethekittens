c9a90e1aa0a7cd70c23be53c4eef8df13af2e979
Imron Hajiahmad <imronha@gmail.com>
2017-02-14 21:49:13 -0800

    first commit

---
+++ KittenProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KittenProject
+//
+//  Created by Imron Hajiahmad on 2/14/17.
+//  Copyright © 2017 Imron Hajiahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print ("hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
